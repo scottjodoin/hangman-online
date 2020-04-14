f67605998bafaf5fe3ea9028c17c2eddcf49bb0d
Scott Jodoin <scottjodoin@gmail.com>
2020-04-13 20:37:14 -0500

    Change information around uselessly for test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //FINISHED: Making the SVG hangman function and the overlay input.
 //NEXT: Change the routing of the submit button through ws? Or should it stay as post?
-//Change index.html to index.ejs and render the view properly in 'previews'. Change the url route.
+//NEXT:Change index.html to index.ejs and render the view properly in 'previews'. Change the url route.
 let express = require('express');
 let app  = express();
 let  fs = require('fs');
